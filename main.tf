2343533667482f928bdfae3f54c00919f1807a22
Lee heechan <a_line1@naver.com>
2022-06-30 21:41:06 +0900

    main.tf 변경사항: S3 Bucket 생성

--- main.tf
+++ main.tf
@@ -10,3 +10,18 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+module "bucket_save_projects" {
+    source = "terraform-aws-modules/s3-bucket/aws"
+
+    bucket ="lee-save-projects-results"
+    # 권한이 있는 자만 접근가능. 기본적으로 Private.
+    acl    = "private"
+
+    # Terraform이 삭제하기 쉽게 함.
+    force_destroy = true
+
+    tags = {
+        "IaCTool" = "Terraform"
+    }
+}
